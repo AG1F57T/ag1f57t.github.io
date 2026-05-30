fa46a562e9cfcf4d52a5be5d4b2472294e1bc71b
WOJTEKPC\wojte <wojtek.kumpel@wp.pl>
2026-05-30 02:10:36 +0200

    labh finish

---
+++ LAB_H/ptw/lab-h/src/Entity/Recipe.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $subject = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $steps = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Ingredients = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): static
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getSteps(): ?string
+    {
+        return $this->steps;
+    }
+
+    public function setSteps(string $steps): static
+    {
+        $this->steps = $steps;
+
+        return $this;
+    }
+
+    public function getIngredients(): ?string
+    {
+        return $this->Ingredients;
+    }
+
+    public function setIngredients(string $Ingredients): static
+    {
+        $this->Ingredients = $Ingredients;
+
+        return $this;
+    }
+}
