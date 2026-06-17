cb8cad37ba3b521acbbfc21d4752eba2c5fc3716
WOJTEKPC\wojte <wojtek.kumpel@wp.pl>
2026-06-17 21:57:47 +0200

    labj fix

--- LAB_J/lab-j/hello.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-name = "Wojciech"
-album = "57780"
-version = sys.version.split()[0]
-location = sys.executable
-
-print(f"Hello {name} ({album}). This environment is using Python version {version} at location {location}.")
